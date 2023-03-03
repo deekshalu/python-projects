fee9a778c554402ee2b4a45d0aad4c106cfb94a1
deekshalu <33671723+deekshalu@users.noreply.github.com>
2023-03-03 13:48:07 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,21 @@
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+bird = input("Enter a species of bird: ")
+house_room = input("Enter a room in a house: ")
+p_verb = input("Enter a verb in the past tense: ")
+verb1 = input("Enter a verb: ")
+name = input("Enter a name of a person: ")
+noun1 = input("Enter a noun: ")
+liquid = input("Enter a type of liquid: ")
+verb_ing1 = input("Enter a verb ending in -ing: ")
+body_part = input("Enter a part of the body: ")
+p_noun = input("Enter a plural noun: ")
+verb_ing2 = input("Enter a verb ending in -ing: ")
+noun2 = input("Enter a noun: ")
+
+madlib = f"It was a {adj1}, cold November day. I woke up to the {adj2} smell of {bird} roasting in the {house_room} downstairs. \
+I {p_verb} down the stairs to see if I could help {verb1} the dinner. My mom said, 'See if {name} needs a fresh {noun1}. ' \
+So I carried a tray of glasses full of {liquid} into the {verb_ing1} room. When I got there, I couldn't believe my \
+{body_part}! There were {p_noun} {verb_ing2} on the {noun2}!"
+
+print(madlib)
